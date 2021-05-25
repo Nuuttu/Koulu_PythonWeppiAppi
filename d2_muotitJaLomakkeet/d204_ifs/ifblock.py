4f5dd7d9c8fa7255e6f83edc92d95566b6e8b29a
Nuuttu <34184542+Nuuttu@users.noreply.github.com>
2021-05-25 12:55:59 +0300

    for if weekdays

--- d2_muotitJaLomakkeet/d204_ifs/ifblock.py
+++ d2_muotitJaLomakkeet/d204_ifs/ifblock.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from flask import Flask, render_template
 app = Flask(__name__)
 
@@ -7,12 +8,25 @@ def base():
     bikes = []
     number = 0
     title = ""
+    day = datetime.weekday(datetime.now())
+    weekdays = [
+        "Monday",
+        "Tuesday",
+        "Wednesday",
+        "Thursday",
+        "Friday",
+        "Saturday",
+        "Sunday"
+    ]
+    
     return render_template(
     'base.html', 
     cars=cars, 
     bikes=bikes, 
     number=number,
-    title=title)
+    title=title,
+    day=day,
+    weekdays=weekdays)
     
 if __name__=="__main__":
     app.run(debug=True)

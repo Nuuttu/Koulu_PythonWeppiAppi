892c40342542e2841260912b9fb9e8be05272826
Nuuttu <34184542+Nuuttu@users.noreply.github.com>
2021-05-25 11:49:15 +0300

    ifblocks

---
+++ d2_muotitJaLomakkeet/d204_ifs/ifblock.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route('/')
+def base():
+    cars = ['ferrari', 'maserati', 'mclaren']
+    bikes = []
+    number = 0
+    title = "ifblockstitle"
+    return render_template(
+    'base.html', 
+    cars=cars, 
+    bikes=bikes, 
+    number=number,
+    title=title)
+    
+if __name__=="__main__":
+    app.run(debug=True)

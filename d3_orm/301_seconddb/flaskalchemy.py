3261bbe87edc577562b79e1d268af5755a42ed13
Nuuttu <34184542+Nuuttu@users.noreply.github.com>
2021-05-26 12:24:41 +0300

    d

--- d3_orm/301_seconddb/flaskalchemy.py
+++ d3_orm/301_seconddb/flaskalchemy.py
@@ -22,6 +22,7 @@ def initMe():
 @app.route('/')
 def base():
     coffees = Coffee.query.all()
+
     return render_template("index.html", coffees=coffees)
 
 if __name__=="__main__":
---
+++ d3_orm/302_modelform/modelform.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+app=Flask(__name__)
+db = SQLAlchemy(app)
+
+class Comment(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.String, nullable=False)
+    name = db.Column(db.String, nullable=False)
+
+@app.before_first_request
+def initMe():
+    db.create_all()
+
+    comment = Comment(text="This is a comment", name="Tuomo")
+    db.session.add(comment)
+    db.session.add(Comment(text="This is the second comment", name="Tarmo"))
+
+    db.session.commit()
+
+@app.route('/')
+def base():
+    comments = Comment.query.all()
+    return render_template("index.html", comments=comments)
+
+if __name__=="__main__":
+    app.run(debug=True)

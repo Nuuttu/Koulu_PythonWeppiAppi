95e91b5cc462db2c46f3aebb8a3842828bb6bc17
Nuuttu <34184542+Nuuttu@users.noreply.github.com>
2021-05-26 12:43:19 +0300

    302

--- d3_orm/302_modelform/modelform.py
+++ d3_orm/302_modelform/modelform.py
@@ -1,13 +1,21 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 
+from flask_wtf import FlaskForm
+from wtforms.ext.sqlalchemy.orm import model_form
+
 app=Flask(__name__)
+app.secret_key = "hVzUfSLFCoRRPcLv07PW0lethoLHUD"
 db = SQLAlchemy(app)
 
 class Comment(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     text = db.Column(db.String, nullable=False)
     name = db.Column(db.String, nullable=False)
+    place = db.Column(db.String)
+
+CommentForm = model_form(Comment,
+    base_class=FlaskForm, db_session=db.session)
 
 @app.before_first_request
 def initMe():
@@ -18,6 +26,11 @@ def initMe():
     db.session.add(Comment(text="This is the second comment", name="Tarmo"))
 
     db.session.commit()
+
+@app.route('/new', methods=["GET", "POST"])
+def addForm():
+    form = CommentForm()
+    return render_template("new.html", form=form)
 
 @app.route('/')
 def base():
